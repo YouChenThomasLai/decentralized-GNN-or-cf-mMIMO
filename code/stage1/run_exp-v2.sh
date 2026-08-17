46f6dc39a4dc01a665d818d5855210cf981fd11a
youchen <youchen.thomas.lai@gmail.com>
2026-08-17 17:16:33 +0800

    copy code for stage1

---
+++ code/stage1/run_exp-v2.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+
+
+##################################### vary M (batch_size 8) ##################################### 
+python trainer_2.py --M 1 --N 30 --L 4 --K 8 --pmax_dbm 15 --batch_size 8 --runs 1 --device cuda:0 --out_dir results_batch_8_BS-radius_200_RIS-radius_100_vary_M
+sleep 5
+
+python trainer_2.py --M 2 --N 30 --L 4 --K 8 --pmax_dbm 15 --batch_size 8 --runs 1 --device cuda:0 --out_dir results_batch_8_BS-radius_200_RIS-radius_100_vary_M
+sleep 5
+
+python trainer_2.py --M 3 --N 30 --L 4 --K 8 --pmax_dbm 15 --batch_size 8 --runs 1 --device cuda:0 --out_dir results_batch_8_BS-radius_200_RIS-radius_100_vary_M
+sleep 5
+
+python trainer_2.py --M 4 --N 30 --L 4 --K 8 --pmax_dbm 15 --batch_size 8 --runs 1 --device cuda:0 --out_dir results_batch_8_BS-radius_200_RIS-radius_100_vary_M
+sleep 5
+
+python trainer_2.py --M 5 --N 30 --L 4 --K 8 --pmax_dbm 15 --batch_size 8 --runs 1 --device cuda:0 --out_dir results_batch_8_BS-radius_200_RIS-radius_100_vary_M
+sleep 5
+
+##################################### vary Pmax in dBm (batch_size 8) #####################################
+python trainer_2.py --M 2 --N 30 --L 4 --K 8 --pmax_dbm 5 --batch_size 8 --runs 1 --device cuda:0 --out_dir results_batch_8_BS-radius_200_RIS-radius_100_vary_Pmax
+sleep 5
+
+python trainer_2.py --M 2 --N 30 --L 4 --K 8 --pmax_dbm 10 --batch_size 8 --runs 1 --device cuda:0 --out_dir results_batch_8_BS-radius_200_RIS-radius_100_vary_Pmax
+sleep 5
+
+python trainer_2.py --M 2 --N 30 --L 4 --K 8 --pmax_dbm 15 --batch_size 8 --runs 1 --device cuda:0 --out_dir results_batch_8_BS-radius_200_RIS-radius_100_vary_Pmax
+sleep 5
+
+python trainer_2.py --M 2 --N 30 --L 4 --K 8 --pmax_dbm 20 --batch_size 8 --runs 1 --device cuda:0 --out_dir results_batch_8_BS-radius_200_RIS-radius_100_vary_Pmax
+sleep 5
+
+python trainer_2.py --M 2 --N 30 --L 4 --K 8 --pmax_dbm 25 --batch_size 8 --runs 1 --device cuda:0 --out_dir results_batch_8_BS-radius_200_RIS-radius_100_vary_Pmax
+sleep 5
+
+python trainer_2.py --M 2 --N 30 --L 4 --K 8 --pmax_dbm 30 --batch_size 8 --runs 1 --device cuda:0 --out_dir results_batch_8_BS-radius_200_RIS-radius_100_vary_Pmax
+sleep 5
+
+python trainer_2.py --M 2 --N 30 --L 4 --K 8 --pmax_dbm 35 --batch_size 8 --runs 1 --device cuda:0 --out_dir results_batch_8_BS-radius_200_RIS-radius_100_vary_Pmax
+sleep 5
